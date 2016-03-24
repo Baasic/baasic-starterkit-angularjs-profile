bc2a20a30feda066af7979f46f833bd63441b150
Ivan Pozderac <ivan.pozderac@mono-software.com>
2016-03-24 10:00:49 +0100

    - rename themes, set default theme to full-profile in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ var htmlminOpts = {
 };
 
 var baseUrl = g.util.env.baseUrl || '/';
-var theme = g.util.env.theme || 'minimal-profile';
+var theme = g.util.env.theme || 'full-profile';
 
 //Plugins
 var atImport = require('postcss-import');
